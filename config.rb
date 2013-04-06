8162dae086edc6a0b08e0ce8e8e8b20f7e62d630
Martino <martino.buffolino@viacommix.com>
2013-04-06 11:55:47 -0400

    updated config

--- config.rb
+++ config.rb
@@ -3,7 +3,6 @@ require 'compass'
 require 'compass-h5bp'
 require 'susy'
 
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
@@ -13,7 +12,10 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-# output_style = :compact
+output_style = :compact
+
+# remove comments production ready
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
